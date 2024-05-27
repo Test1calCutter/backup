8d1a298228fef7e714019f42a19d0c29f902db23
Noel <108223454+Test1calCutter@users.noreply.github.com>
2024-05-27 19:10:57 +0200

    Add files via upload

---
+++ v2/js/menu.js
@@ -0,0 +1,102 @@
+var _0x3b1dcd = _0x3a13;
+(function (_0x1ddfb6, _0x8cdd57) {
+    var _0x35f1f2 = _0x3a13;
+    var _0x549359 = _0x1ddfb6();
+    while (!![]) {
+        try {
+            var _0xd2a40a = -parseInt(_0x35f1f2(0x11f)) / 0x1 + parseInt(_0x35f1f2(0x12a)) / 0x2 + -parseInt(_0x35f1f2(0x127)) / 0x3 + -parseInt(_0x35f1f2(0x12e)) / 0x4 + parseInt(_0x35f1f2(0x11e)) / 0x5 * (parseInt(_0x35f1f2(0x11a)) / 0x6) + -parseInt(_0x35f1f2(0x132)) / 0x7 * (parseInt(_0x35f1f2(0x136)) / 0x8) + parseInt(_0x35f1f2(0x12c)) / 0x9;
+            if (_0xd2a40a === _0x8cdd57) {
+                break;
+            } else {
+                _0x549359['push'](_0x549359['shift']());
+            }
+        } catch (_0xac8665) {
+            _0x549359['push'](_0x549359['shift']());
+        }
+    }
+}(_0x206e, 0xeee2a));
+jQuery(document)[_0x3b1dcd(0x128)](function (_0x790976) {
+    var _0x23efc1 = _0x3b1dcd;
+    var _0x15ba73 = 0x492;
+    if (_0x790976(window)[_0x23efc1(0x11d)]() > _0x15ba73) {
+        var _0x5c8078 = _0x790976('.box-header')[_0x23efc1(0x130)]();
+        _0x790976(window)['on'](_0x23efc1(0x11b), { 'previousTop': 0x0 }, function () {
+            var _0x1746ae = _0x23efc1;
+            var _0x2125c6 = _0x790976(window)['scrollTop']();
+            if (_0x2125c6 < this[_0x1746ae(0x135)]) {
+                if (_0x2125c6 > 0x0 && _0x790976(_0x1746ae(0x121))[_0x1746ae(0x11c)]('is-fixed')) {
+                    _0x790976('.box-header')['addClass'](_0x1746ae(0x120));
+                } else {
+                    _0x790976(_0x1746ae(0x121))[_0x1746ae(0x126)]('is-visible\x20is-fixed');
+                }
+            } else {
+                _0x790976('.box-header')[_0x1746ae(0x126)]('is-visible');
+                if (_0x2125c6 > _0x5c8078 && !_0x790976('.box-header')['hasClass'](_0x1746ae(0x124)))
+                    _0x790976(_0x1746ae(0x121))['addClass'](_0x1746ae(0x124));
+            }
+            this[_0x1746ae(0x135)] = _0x2125c6;
+        });
+    }
+    _0x790976(_0x23efc1(0x123))['on']('click', function () {
+        var _0x82cf85 = _0x23efc1;
+        _0x790976('.box-menu-icon')[_0x82cf85(0x125)](_0x82cf85(0x133));
+        _0x790976(_0x82cf85(0x121))[_0x82cf85(0x125)](_0x82cf85(0x12b));
+        if (_0x790976(_0x82cf85(0x129))[_0x82cf85(0x11c)](_0x82cf85(0x120))) {
+            _0x790976('.box-primary-nav')[_0x82cf85(0x126)]('is-visible')[_0x82cf85(0x134)](_0x82cf85(0x131), function () {
+                var _0x3b0cb1 = _0x82cf85;
+                _0x790976(_0x3b0cb1(0x122))['removeClass'](_0x3b0cb1(0x12f));
+            });
+        } else {
+            _0x790976(_0x82cf85(0x129))[_0x82cf85(0x12d)](_0x82cf85(0x120))[_0x82cf85(0x134)]('webkitTransitionEnd\x20otransitionend\x20oTransitionEnd\x20msTransitionEnd\x20transitionend', function () {
+                var _0x1a9140 = _0x82cf85;
+                _0x790976(_0x1a9140(0x122))[_0x1a9140(0x12d)](_0x1a9140(0x12f));
+            });
+        }
+    });
+});
+function _0x3a13(_0x5edb26, _0x53460c) {
+    var _0x206e72 = _0x206e();
+    _0x3a13 = function (_0x3a1328, _0x2e349e) {
+        _0x3a1328 = _0x3a1328 - 0x11a;
+        var _0x55e4ca = _0x206e72[_0x3a1328];
+        return _0x55e4ca;
+    };
+    return _0x3a13(_0x5edb26, _0x53460c);
+}
+function _0x206e() {
+    var _0x5b73f6 = [
+        '.box-header',
+        'body',
+        '.box-primary-nav-trigger',
+        'is-fixed',
+        'toggleClass',
+        'removeClass',
+        '2579559JShXDH',
+        'ready',
+        '.box-primary-nav',
+        '2373916mkECfM',
+        'menu-is-open',
+        '3013425MDhThY',
+        'addClass',
+        '1993568jRlBgc',
+        'overflow-hidden',
+        'height',
+        'webkitTransitionEnd\x20otransitionend\x20oTransitionEnd\x20msTransitionEnd\x20transitionend',
+        '7SBhMDK',
+        'is-clicked',
+        'one',
+        'previousTop',
+        '2054168cNwIoA',
+        '8984454lkkzCV',
+        'scroll',
+        'hasClass',
+        'width',
+        '5ItuIAo',
+        '425702DLWELZ',
+        'is-visible'
+    ];
+    _0x206e = function () {
+        return _0x5b73f6;
+    };
+    return _0x206e();
+}
